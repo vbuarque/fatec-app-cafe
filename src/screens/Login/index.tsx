9412202b590110478b0e7f3defe550a0b5fc9dc8
vbuarque <viniciusbuarque17@gmail.com>
2022-09-22 00:35:52 -0300

    FEAT: Criando a tela de login

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import {Container, Title} from './styles';
+import React from "react";
+import { Container, Title, Image, ButtonLogin, TitleButton} from "./styles";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+import Logo from "../../assets/images/LogoMiau.svg";
+
+import ImageCat from "../../assets/images/catImageLogin.png";
 
 export default function Login() {
   return (
     <Container>
-      <Title>login</Title>
+      <Logo />
+      <Image source={ImageCat} />
+      <Title>Tome café enquanto faz carinho nos gatinhos.</Title>
+
+      <ButtonLogin>
+      <FontAwesomeIcon icon={faGoogle} color="white"/>
+        <TitleButton>
+          Acesse com o Google
+        </TitleButton>
+      </ButtonLogin>
+
+      <ButtonLogin>
+      <FontAwesomeIcon icon={faFacebookF} color="white"/>
+        <TitleButton>
+          Acesse com o Facebook
+        </TitleButton>
+      </ButtonLogin>
     </Container>
   );
 }
--- src/screens/Login/styles.ts
+++ src/screens/Login/styles.ts
@@ -2,11 +2,38 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
     flex: 1;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
 `;
 
 export const Title = styled.Text`
     font-size: 24px;
-    color: #000;
+    color: #29292E;
+    /* margin-bottom: 104px; */
+`;
+
+export const Image = styled.Image`
+    width: 256px;
+    height: 277px;
+    /* margin: 78px 0 78px 0; */
+`;
+
+export const TitleButton = styled.Text`
+    font-size: 16px;
+    color: #f8f8ff;
+`;
+
+export const ButtonLogin = styled.TouchableOpacity`
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 232px;
+    height: 48px;
+    
+    padding: 8px 16px 8px 16px;
+
+    background-color: #F292A9;
+
+    border-radius: 8px;
 `;
