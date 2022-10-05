fca3e2bf78a362af0e2a033126cdd29370da1e67
vbuarque <viniciusbuarque17@gmail.com>
2022-10-04 23:21:16 -0300

    FEAT: Adicionando a tela home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+
+import {Text} from "react-native";
+import {Container} from "./styles";
+
+export default function Home() {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+}
